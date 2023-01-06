cebc9fd98939166624fe8ff2ae0b9a21340db0fb
Mukesh Buwade <mukesh@newrise.in>
2023-01-06 11:17:06 +0530

    responsiveness done

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -14,6 +14,53 @@ const tools = [
     "Redux", "BitBucket", "Git", "GitHub", "Postman", "Android Studio", 'VS Code', "Canva", "Figma"
 ]
 
+let experienceArr = [
+    {
+        image: "../images/nrt-logo.png",
+        duration: "Oct,2021 - Present",
+        position: "Jr. React Native Developer",
+        name: "NewRise Technosys Pvt Ltd",
+        location: "Bhopal (MP), India",
+        description: "I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
+    }
+]
+let internshipArr = [
+    {
+        image: "../images/nrt-logo.png",
+        duration: "Feb,2019 - Jul,2019",
+        position: "Full Stack Development",
+        name: "HPE Education Service",
+        location: "Bhopal (MP), India",
+        description: "I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
+    }
+]
+let educationArr = [
+    {
+        image: "../images/nrt-logo.png",
+        duration: "2017 - 2021",
+        position: "Bachelor of Technology",
+        name: "Computer Science and Engineering",
+        location: "RGPV University, Bhopal",
+        description: "I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
+    },
+    {
+        image: "../images/nrt-logo.png",
+        duration: "2015 - 2017",
+        position: "12TH",
+        name: "Maths(Science)",
+        location: "MP Board, Bhopal, India",
+        description: "I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
+    },
+    {
+        image: "../images/nrt-logo.png",
+        duration: "2013 - 2015",
+        position: "10TH",
+        name: "All Subject",
+        location: "MP Board, Bhopal, India",
+        description: "I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
+    }
+]
+
 const Resume = () => {
     return (
         <div id="basecontainer">
@@ -23,60 +70,59 @@ const Resume = () => {
                     <p className="Experience">Experience</p>
                     <NavLink to="/login">
                         <button >
-                            Dowanload CV
+                            Download CV
                         </button>
                     </NavLink>
                 </div>
-                <Experience
-                    company_logo={"../images/nrt-logo.png"}
-                    time={"Oct2021 - Present"}
-                    position={"Jr. React Native Developer"}
-                    componyName={"NewRise Technosys Pvt Ltd"}
-                    location={"Bhopal (MP), India"}
-                    description={"I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."}
-                />
+                {
+                    experienceArr.map((e, i) => (
+                        <Experience
+                            company_logo={e.image}
+                            time={e.duration}
+                            position={e.position}
+                            componyName={e.name}
+                            location={e.location}
+                            description={e.description}
+                        />
+                    ))
+                }
 
-                 {/* -------------------internship section ----------------- */}
-                 <div className="btnContainer" >
+
+                {/* -------------------internship section ----------------- */}
+                <div className="btnContainer" >
                     <p className="Experience">Internship</p>
                 </div>
 
-                <Experience
-                    time={"2017 - 2021"}
-                    position={"Full Stack Development"}
-                    componyName={"HPE Education Service"}
-                    location={"Bhopal"}
-                    description={"I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."}
-                />
+                {
+                    internshipArr.map((e, i) => (
+                        <Experience
+                            company_logo={e.image}
+                            time={e.duration}
+                            position={e.position}
+                            componyName={e.name}
+                            location={e.location}
+                            description={e.description}
+                        />
+                    ))
+                }
 
                 {/* -------------------education section ----------------- */}
                 <div className="btnContainer" >
                     <p className="Experience">Education</p>
                 </div>
 
-                <Experience
-                    time={"2017 - 2021"}
-                    position={"Bachelor of Technology"}
-                    componyName={"Computer Science and Engineering"}
-                    location={"RGPV University, Bhopal"}
-                    description={"I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."}
-                />
-
-                <Experience
-                    time={"2015 - 2017"}
-                    position={"12TH"}
-                    componyName={"Maths(Science)"}
-                    location={"M.P. Board"}
-                    description={"I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."}
-                />
-
-                <Experience
-                    time={"2013 - 2015"}
-                    position={"10TH"}
-                    componyName={"All Subject"}
-                    location={"M. P. Board"}
-                    description={"I'm a paragraph. Click here to add your own text and edit me.  It’s easy. Just click “Edit Text” or double click me to add your own content  and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."}
-                />
+                {
+                    educationArr.map((e, i) => (
+                        <Experience
+                            company_logo={e.image}
+                            time={e.duration}
+                            position={e.position}
+                            componyName={e.name}
+                            location={e.location}
+                            description={e.description}
+                        />
+                    ))
+                }
 
                 {/* -------------------skill section ----------------- */}
                 <div className="btnContainer" >
